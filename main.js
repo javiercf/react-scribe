d94177e764793f10cb2b4cc7ac48f0f9571913ab
javiercf <javiercferrans@gmail.com>
2016-05-11 03:55:09 -0300

    fix dist

---
+++ main.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import ScribeEditor from '../src'
+
+const ShowCase = props => {
+  return (
+    <div className='sc-showCase'>
+      { props.demoContent }
+    </div>
+    )
+};
+
+class DemoApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'hello world'
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(value) {
+    this.setState({ text: value });
+  }
+
+  render() {
+    return (
+      <div>
+        <ScribeEditor defaultValue='hello world' onChange={this.handleChange} />
+        <ShowCase demoContent={this.state.text} />
+      </div>
+    )
+  }
+}
+
+render(
+  <DemoApp />,
+  document.getElementById('main')
+);
