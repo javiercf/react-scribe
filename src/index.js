ed50de4ce09872165146f02dfecca0548a16e0e7
Javier <javier.cubides@avenida.com>
2016-05-13 18:10:41 -0300

    fixing linting issues, added Proptypes

--- src/index.js
+++ src/index.js
@@ -1,61 +1,23 @@
+import React, { Component, PropTypes } from 'react';
 import _ from 'lodash';
+import { findDOMNode } from 'react-dom';
 import optionMap from './optionMap';
-import React from 'react';
 import Scribe from 'scribe-editor';
+import ScribeToolbar from './ToolBar';
 import Toolbar from 'scribe-plugin-toolbar';
-
 /**
  * Default Options
  * commands: array of desired toolbar commands
  */
 
-const defaultOptions = {
-  'commands': ['blockquote', 'code', 'h1', 'h2', 'h3', 'h4', 'h5', 'linkPrompt',
-    'unlink', 'ol', 'ul']
-};
-
-/**
- * Toolbar Component
- */
-
-const ScribeToolbar = props => {
-  let toolBarOptions = [];
-  for (const i in props.config) {
-    toolBarOptions.push(
-      <button data-command-name={ props.config[i]['command'] } key={ i } >
-        <i className={ `fa ${props.config[i]['display']}` } />
-      </button>
-    );
-  }
-
-  return (
-    <div className='sc-toolbar'>
-      <button data-command-name='bold'>
-        <i className='fa fa-bold' />
-      </button>
-      <button data-command-name='italic'>
-        <i className='fa fa-italic' />
-      </button>
-      <button data-command-name='underline'>
-        <i className='fa fa-underline' />
-      </button>
-      { toolBarOptions }
-      <button data-command-name='undo'>
-        <i className='fa fa-undo' />
-      </button>
-      <button data-command-name='redo'>
-        <i className='fa fa-repeat' />
-      </button>
-    </div>
-  );
-};
-
+const defaultOptions = ['blockquote', 'code', 'h2', 'linkPrompt',
+    'unlink', 'ol', 'ul'];
 
 /**
  * Main editor component
  */
 
-class ScribeEditor extends React.Component {
+class ScribeEditor extends Component {
   constructor(props) {
     super(props);
     this.parseConfig = this.parseConfig.bind(this);
@@ -69,9 +31,9 @@ class ScribeEditor extends React.Component {
 
   // Bind scribe to Component and include Toolbar commands and options
   componentDidMount() {
-    const editor = this.editor;
+    const editor = findDOMNode(this.refs.editor);
     const scribe = new Scribe(editor);
-    const toolbarElement = this.toolbar;
+    const toolbarElement = findDOMNode(this.refs.toolbar);
     for (const i in this.parseConfig()['plugins']) {
       scribe.use(this.parseConfig()['plugins'][i]);
     }
@@ -98,7 +60,7 @@ class ScribeEditor extends React.Component {
       'toolbarElements': {},
       'plugins': []
     };
-    config.commands.forEach( cmd => {
+    config.forEach( cmd => {
       const command = _.get(optionMap, cmd);
       if (command) {
         if (command.hasOwnProperty('action')) {
@@ -124,13 +86,29 @@ class ScribeEditor extends React.Component {
     return (
       <div className='sc-container'>
         <ScribeToolbar config={ this.parseConfig()['toolbarElements'] }
-          ref={ c => this.toolbar = c }
+          ref='toolbar'
         />
-        <div className='sc-editor' ref={ c => this.editor = c } />
+        <div className='sc-editor' ref='editor' />
       </div>
     );
   }
 }
+
+ScribeEditor.propTypes = {
+  config: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.shape({
+        command: PropTypes.string.isRequired,
+        display: PropTypes.string.isRequired,
+        action: PropTypes.func.isRequired
+      })
+    ])
+  ),
+  defaultValue: PropTypes.string,
+  onChange: PropTypes.func,
+  value: PropTypes.string
+};
 
 ScribeEditor.defaultProps = {
   config: defaultOptions
